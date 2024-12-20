9fb63755755abd5fa6aae5b94bf63d90e5064699
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-12-20 12:21:21 -0500

    :recycle: add idiomatic version

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,25 +1,21 @@
-use std::collections::HashMap;
-
 pub fn raindrops(n: u32) -> String {
-    let closure_vec: Vec<Box<dyn Fn(u32) -> bool>> = vec![
-        Box::new(|n: u32| n % 3 == 0),
-        Box::new(|n: u32| n % 5 == 0),
-        Box::new(|n: u32| n % 7 == 0),
-    ];
-    let mut words: HashMap<u32, String> = HashMap::new();
-    words.insert(0, String::from("Pling"));
-    words.insert(1, String::from("Plang"));
-    words.insert(2, String::from("Plong"));
-    let mut result = String::new();
-    for (index, closure) in closure_vec.iter().enumerate() {
-        if closure(n) {
-            if let Some(word) = words.get(&(index as u32)) {
-                result += word;
-            }
-        }
+    let words = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
+    let result: String = words
+        .iter()
+        .filter_map(
+            |&(number, word)| {
+                if n % number == 0 {
+                    Some(word)
+                } else {
+                    None
+                }
+            },
+        )
+        .collect();
+
+    if result.is_empty() {
+        n.to_string()
+    } else {
+        result
     }
-    if result.len() == 0 {
-        result = format!("{}", n).to_string();
-    }
-    result
 }
