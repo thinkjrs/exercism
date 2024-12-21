f07c3a0d3ee70ee0ff8d6c639bf64fd1dce33e85
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-12-21 13:32:05 -0500

    :recycle: add naive solution

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,35 +1,43 @@
-use std::{collections::HashMap, default};
+use std::collections::HashMap;
 
-fn split_in_half_by_chars(s: &str) -> (String, String) {
-    let chars: Vec<char> = s.chars().collect();
-    let mid = chars.len() / 2;
-
-    let first_half: String = chars[..mid].iter().collect();
-    let second_half: String = chars[mid..].iter().collect();
-
-    (first_half, second_half)
-}
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let default_matches = HashMap::from([('{', '}'), ('[', ']'), ('(', ')')]);
-    let results = string
+    let openings = ['{', '[', '('];
+    let closings = ['}', ']', ')'];
+    let default_matches: HashMap<char, char> =
+        openings.into_iter().zip(closings.into_iter()).collect();
+    let mut stack: Vec<char> = vec![];
+    let matches = string
         .chars()
         .filter_map(|s| match s {
             '{' | '[' | '(' | '}' | ']' | ')' => Some(s),
             _ => None,
         })
         .collect::<String>();
-    if results.len() % 2 != 0 {
+    if matches.len() % 2 != 0 {
         return false;
     }
-    let (lhs, rhs) = split_in_half_by_chars(&results);
-    // split the string and iterate pairwise
-    for (left, right) in lhs.chars().zip(rhs.chars().rev()) {
-        if let Some(matched) = default_matches.get(&left) {
-            if matched != &right {
-                println!("Oh no not matched! {} {}", matched, right);
+    for c in matches.chars() {
+        // check if opening and push to stack
+        if openings.contains(&c) {
+            stack.push(c);
+            continue;
+        }
+        if closings.contains(&c) {
+            if let Some(opening) = stack.pop() {
+                if let Some(expected_closing) = default_matches.get(&opening) {
+                    if expected_closing != &c {
+                        return false;
+                    }
+                }
+            } else {
                 return false;
             }
         }
     }
-    true
+
+    if stack.len() > 0 {
+        false
+    } else {
+        true
+    }
 }
