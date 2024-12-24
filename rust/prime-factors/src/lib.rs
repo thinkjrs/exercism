c8c2f96bf25a3634da27a2d68e7977ff476bee73
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-12-23 19:56:34 -0500

    :recycle: refactor to make slightly more idiomatic

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,20 +1,29 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut factors = vec![];
+    let mut factors = Vec::new();
     let mut n = n;
+
     while n % 2 == 0 {
         factors.push(2);
         n /= 2;
     }
+    let max_factor_limit = (n as f64).sqrt() as u64;
+    let range = (3..=max_factor_limit).step_by(2);
 
-    for i in (3..=(n as f64).sqrt() as u64).step_by(2) {
+    factors.extend(range.flat_map(|i| {
+        let mut tmp_factors = vec![0u64; max_factor_limit as usize];
+        let mut count = 0;
+
         while n % i == 0 {
-            factors.push(i);
+            tmp_factors[count] = i;
+            count += 1;
             n /= i;
         }
-    }
+        tmp_factors[..count].to_vec()
+    }));
 
     if n > 2 {
         factors.push(n);
     }
+
     factors
 }
