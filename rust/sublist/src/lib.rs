673040121f6ab399cc15894a56d9edb9569bf213
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-12-25 21:23:52 -0500

    :recycle: improve to make more idiomatic

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,33 +6,15 @@ pub enum Comparison {
     Unequal,
 }
 
-impl Comparison {
-    pub fn is_equal(&self) -> bool {
-        self == &Comparison::Equal
-    }
-    pub fn is_sublist(&self) -> bool {
-        self == &Comparison::Sublist
-    }
-    pub fn is_superlist(&self) -> bool {
-        self == &Comparison::Superlist
-    }
-    pub fn is_unequal(&self) -> bool {
-        self == &Comparison::Unequal
-    }
-}
-
 pub fn is_sublist<T: PartialEq>(small: &[T], big: &[T]) -> bool {
     small.is_empty() || big.windows(small.len()).any(|window| window == small)
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list == _second_list {
-        Comparison::Equal
-    } else if is_sublist(_first_list, _second_list) {
-        Comparison::Sublist
-    } else if is_sublist(_second_list, _first_list) {
-        Comparison::Superlist
-    } else {
-        Comparison::Unequal
+    match (_first_list, _second_list) {
+        _ if _first_list == _second_list => Comparison::Equal,
+        _ if is_sublist(_first_list, _second_list) => Comparison::Sublist,
+        _ if is_sublist(_second_list, _first_list) => Comparison::Superlist,
+        _ => Comparison::Unequal,
     }
 }
