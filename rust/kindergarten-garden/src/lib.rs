4e973a3d73a6b41deb8e7e9f8bc48271f8c5af21
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-12-30 14:43:16 -0500

    :recycle: add optimized idiomatic solution

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -7,34 +7,33 @@ pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
         ('R', "radishes"),
         ('V', "violets"),
     ]);
+
     let students = [
         "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
         "Kincaid", "Larry",
     ];
-    let mut result: Vec<&'static str> = Vec::new();
-    let mut student_index: usize = 0;
-    // split the string diagram by \n
-    if let Some((first, second)) = diagram.split_once("\n") {
-        // iterate through each simultaneously using zip
-        for (index, (front, back)) in first.chars().zip(second.chars()).enumerate() {
-            if index.rem_euclid(2) == 0 {
-                student_index += 1;
-            };
-            // match if the student is correct, counting by twos
-            if students[student_index - 1] == student {
-                // match the pattern to plants
-                if let Some(plant_type) = plant_types.get(&front) {
-                    result.push(plant_type);
-                }
 
-                if let Some(plant_type) = plant_types.get(&back) {
-                    result.push(plant_type);
-                }
-            }
-        }
-        result.swap(1, 2);
-        result
-    } else {
+    let rows: Vec<&str> = diagram.split('\n').collect();
+
+    if rows.len() % 2 != 0 {
         panic!("Diagram incomplete - no newline found!");
     }
+
+    // get index of student
+    let student_index = students
+        .iter()
+        .position(|s| *s == student)
+        .expect("Student not found!");
+
+    let result: Vec<&'static str> = rows
+        .iter()
+        .flat_map(|row| {
+            row.chars()
+                .skip(student_index * 2)
+                .take(2)
+                .map(|plant| *plant_types.get(&plant).expect("Invalid plant key!"))
+        })
+        .collect();
+
+    result
 }
