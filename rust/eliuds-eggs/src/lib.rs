3afbfecbb29ed53002cc7791d95991e4d07f7416
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-12-30 19:40:14 -0500

    :sparkles: add solution for eliuds-eggs using iterators

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+    //todo!("count the eggs in {display_value}")
+    let display_value_binary_chars: Vec<char> = format!("{:b}", display_value).chars().collect();
+    let num_eggs: usize = display_value_binary_chars
+        .iter()
+        .filter(|&c| *c == '1')
+        .count();
+
+    num_eggs
 }
