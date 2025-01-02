a010ae567ab2376bcbd4074fabb5da257275198f
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2025-01-02 08:16:43 -0500

    :recycle: update to make more idiomatic and efficient avoiding the initial vector

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn egg_count(display_value: u32) -> usize {
-    //todo!("count the eggs in {display_value}")
-    let display_value_binary_chars: Vec<char> = format!("{:b}", display_value).chars().collect();
-    let num_eggs: usize = display_value_binary_chars
-        .iter()
-        .filter(|&c| *c == '1')
+    let num_eggs: usize = format!("{:b}", display_value)
+        .chars()
+        .filter(|c| *c == '1')
         .count();
 
     num_eggs
