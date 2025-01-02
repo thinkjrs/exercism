bb34942d8c39da854714012d888314eb5321ce9e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2025-01-02 10:47:52 -0500

    :recycle: remove some repition and simplify logic using .all on chars()

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,26 +2,26 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut result: HashSet<&'a str> = HashSet::new();
+
+    let lowercase_word = word.to_lowercase();
+
     // loop through possible_anagrams
-    for anagram in possible_anagrams {
+    for &anagram in possible_anagrams {
+        let lowercase_anagram = anagram.to_lowercase();
+
         // given each possible anagram, ask if all characters in word are present in anagram
-        if anagram.to_lowercase() != word.to_lowercase() && anagram.len() == word.len() {
-            let mut word = word.to_string().to_lowercase();
-            let test = anagram
-                .chars()
-                .filter(|&c| {
-                    let lower_c = c.to_lowercase().next().unwrap();
-                    if word.contains(lower_c) {
-                        if let Some(pos) = word.find(lower_c) {
-                            word.remove(pos);
-                            return true;
-                        }
-                    }
-                    false
-                })
-                .collect::<String>();
-            if test.as_str().to_lowercase() == *anagram.to_lowercase() {
-                result.insert(*anagram);
+        if lowercase_anagram != lowercase_word && lowercase_anagram.len() == lowercase_word.len() {
+            let mut word_chars = lowercase_word.clone();
+            let is_anagram = lowercase_anagram.chars().all(|c| {
+                if let Some(pos) = word_chars.find(c) {
+                    word_chars.remove(pos);
+                    return true;
+                }
+                false
+            });
+
+            if is_anagram {
+                result.insert(anagram);
             }
         }
     }
