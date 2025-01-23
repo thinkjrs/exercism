3ccd3780df0677d881820dd675c7ee2161c1cc4c
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2025-01-23 09:00:34 -0500

    :recycle: add idiomatic solution to luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,44 +1,24 @@
 /// Check a Luhn checksum.
-pub fn double_digit(mut digit: u32) -> u32 {
-    digit *= 2;
-    if digit > 9 {
-        digit -= 9;
-    }
-    digit
+pub fn double_digit(digit: u32) -> u32 {
+    let doubled = digit << 1;
+    doubled - (doubled > 9) as u32 * 9
 }
 pub fn is_valid(code: &str) -> bool {
-    //todo!("Is the Luhn checksum for {code} valid?");
-    // Remove all spaces
-    // Check if the length is less than 2, if truthy return false
-    // Check if all characters are digits, if not return false
-    // Initialize sum = 0
-    // Loop from back to front, if the index is even, double the digit
-    //   if doubling the digit results in a number greater than 9, subtract 9
-    //   add to sum
-    // Return if sum % 10 == 0
-    let binding = code.replace(" ", "");
-    let code = binding.chars();
-    if code.as_str().len() < 2 {
-        false
-    } else {
-        let mut sum = 0;
-        for (i, c) in code.rev().enumerate() {
-            if let Some(mut digit) = c.to_digit(10) {
-                if i % 2 == 1 {
-                    digit = double_digit(digit);
-                }
-                println!("digit: {}", digit);
-                println!("acc: {}", sum);
-                println!("sum: {}", sum + digit);
-                sum += digit;
-            } else {
-                return false;
-            }
-        }
-        if sum % 10 == 0 {
-            true
-        } else {
-            false
-        }
+    let code = code.replace(" ", "");
+    if code.len() < 2 {
+        return false;
     }
+    code.chars()
+        .rev()
+        .enumerate()
+        .try_fold(0_u32, |sum, (i, c)| {
+            c.to_digit(10).map(|digit| {
+                sum + if i % 2 == 1 {
+                    double_digit(digit)
+                } else {
+                    digit
+                }
+            })
+        })
+        .map_or(false, |sum| sum % 10 == 0)
 }
